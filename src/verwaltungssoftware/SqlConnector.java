252f96604dff73f90f9554ff1af55f2571713f05
M4rcelNo4ck <32970791+M4rcelNo4ck@users.noreply.github.com>
2017-11-21 16:02:13 +0100

    pull test

--- src/verwaltungssoftware/SqlConnector.java
+++ src/verwaltungssoftware/SqlConnector.java
@@ -309,7 +309,6 @@ public class SqlConnector implements ISql {
             stmtUpdateArtikel.setString(2, eingabe);
             stmtUpdateArtikel.setString(3, id);
             stmtUpdateArtikel.executeUpdate();
-            System.out.println("testcommit");
         } catch (SQLException exc) {
             throw exc;
         }
